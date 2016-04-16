98275f9209c0c30fab0123fdc64b640f3ce50704
mailethiesen <mailethiesen>
2016-04-16 03:18:51 +0000

    added user auth for updating

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.all.paginate(:page => params[:page], :per_page => 3)
@@ -20,10 +20,19 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render text: 'Not Allowed, only the user who created this place can delete it.', status: :forbidden
+    end
   end
 
   def update
     @place = Place.find(params[:id])
+
+    if @place.user != current_user
+      return render text: 'Not Allowed, only the user who created this place can update it.', status: :forbidden
+    end
+    
     @place.update_attributes(place_params)
     redirect_to root_path
   end

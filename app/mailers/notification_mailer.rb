07688520c26f5aa03d68fcb76a9828901b43d211
mailethiesen <mailethiesen>
2016-07-02 19:58:12 +0000

    added emails to pull from db and added @place to pull from db and adjusted subject line and body of email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,11 +1,10 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
   
-  def comment_added
-  # def comment_added(comment)
-    # @place = comment.place
-    # @place_owner = @place.user
-    mail(to: "mailethiesen@gmail.com",
-         subject: "A comment has been added to your place") 
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+    mail(to: @place_owner.email,
+         subject: "A comment has been added to #{@place.name}") 
   end
 end
